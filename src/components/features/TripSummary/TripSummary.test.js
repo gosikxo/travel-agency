46cd032b8062ea52a4d99d53866e1d076366c113
Małgorzata Brożek <gosikbrozek1997@gmail.com>
2020-01-19 19:58:33 +0100

    Add more tests to TripSummary

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -9,8 +9,8 @@ describe('Component TripSummary', () => {
     name: '', 
     cost: '',
     days: 1,
-
   };
+
   it('should render without crashing', () => {
     const id = 'abc';
     const component = shallow(<TripSummary {...defaultProps} id={id}/>);
@@ -44,9 +44,34 @@ describe('Component TripSummary', () => {
     expect(component.find('.details').text().includes(`from ${cost}`)).toEqual(true);
   });
 
-  
-
   it('should throw error without required props', () => {
     expect(() => shallow(<TripSummary />)).toThrow();
+  });
+
+  it('should render tags', () => {
+    const tags = ['nice', 'cool', 'rude'];
+    const component = shallow(<TripSummary 
+      {...defaultProps}
+      tags={tags}
+    />);
+    expect(component.find('.tag').at(0).text()).toEqual(tags[0]);
+    expect(component.find('.tag').at(1).text()).toEqual(tags[1]);
+    expect(component.find('.tag').at(2).text()).toEqual(tags[2]);
+  });
+
+  it('should not render tags if tags are not provided', () => {
+    const component = shallow(<TripSummary 
+      {...defaultProps}
+      tags={undefined}
+    />);
+    expect(component.find('.tag').length).toEqual(0);
+  });
+
+  it('should not render tags if tags are empty list', () => {
+    const component = shallow(<TripSummary 
+      {...defaultProps}
+      tags={[]}
+    />);
+    expect(component.find('.tag').length).toEqual(0);
   });
 });
